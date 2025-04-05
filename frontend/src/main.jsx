b0418fe5283a9b099634088a32376f66a5d62f37
Aditya Zanzad <115469452+aditya-zanzad@users.noreply.github.com>
2025-04-06 04:33:15 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { createRoot } from "react-dom/client"; // Correct import
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import React from "react";
+import { unregister } from './serviceWorker';
 
-const root = createRoot(document.getElementById("root")); // Create root
+const root = createRoot(document.getElementById("root"));
+
+// Unregister service workers
+unregister();
+
+// Render the app
 root.render(
   <BrowserRouter>
     <App />
